47208215944e64dafba061939099337ea8936e5a
Jerry Thimothy J <jerrythimothy@gmail.com>
2019-10-18 00:29:29 +0530

    Unit test credit card breaking scenarios #13

--- src/credit-card/credit-card.controller.spec.ts
+++ src/credit-card/credit-card.controller.spec.ts
@@ -16,20 +16,29 @@ describe('CreditCardController', () => {
 
   describe('add', () => {
     it('should save each credit card and return the list', async () => {
-      const sampleCreditCardInfo = {
+      const sampleCreditCardInfo0 = {
+        id: 0,
         name: 'sss',
-        number: 4539019090394224,
+        number: '4539019090394224',
         limit: 0,
       };
+
+      const sampleCreditCardInfo1 = {
+        id: 0,
+        name: 'sss',
+        number: '4024007176710456',
+        limit: 0,
+      };
+
       const expectedArray = [];
-      let expected = {...sampleCreditCardInfo, id: 1, balance: 0};
+      let expected = {...sampleCreditCardInfo0, id: 1, balance: 0};
       expectedArray.push(expected);
-      let actual = creditCardController.add(sampleCreditCardInfo);
+      let actual = creditCardController.add(sampleCreditCardInfo0);
       expect(actual).toEqual(expected);
 
-      expected = {...sampleCreditCardInfo, id: 2, balance: 0};
+      expected = {...sampleCreditCardInfo1, id: 2, balance: 0};
       expectedArray.push(expected);
-      actual = creditCardController.add(sampleCreditCardInfo);
+      actual = creditCardController.add(sampleCreditCardInfo1);
       expect(actual).toEqual(expected);
 
       const actualArray = creditCardController.getAll();
